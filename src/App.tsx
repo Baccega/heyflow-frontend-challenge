edba8e3374d9c25910a742b0c3563c894e3a8ab7
Sandro Baccega <mail.sandro.baccega@gmail.com>
2024-11-27 15:20:37 +0100

    Add basic project structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,28 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import githubLogo from "./assets/github.svg";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+      <header className="header">
+        <h1>HeyFlow's Frontend Challenge</h1>
+        <h2>By Sandro Baccega</h2>
+      </header>
+      <main>
+        <nav>
+          <a href="/json-explorer/">JSON Explorer</a>
+          <a href="/custom-checkbox/">Custom Checkbox</a>
+        </nav>
+      </main>
+
+      <footer>
+        <a
+          href="https://github.com/Baccega/heyflow-frontend-challenge"
+          target="_blank"
+        >
+          <img src={githubLogo} className="logo" alt="Github" />
         </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      </footer>
     </>
   );
 }
---
+++ src/json-explorer/JsonExplorer.tsx
@@ -0,0 +1,20 @@
+import backIcon from "../assets/back.svg";
+import "../App.css";
+
+function App() {
+  return (
+    <>
+      <header className="backButtonContainer">
+        <nav>
+          <a href="/">
+            <img src={backIcon} className="back" alt="Back" />
+          </a>
+        </nav>
+        <h1>JSON Explorer</h1>
+      </header>
+      <main>ergwergwergwergwerg</main>
+    </>
+  );
+}
+
+export default App;
