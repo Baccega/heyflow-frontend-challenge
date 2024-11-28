eaf87be549f66b5e706df2128b8b3fe84ecb58dc
Sandro Baccega <mail.sandro.baccega@gmail.com>
2024-11-28 10:34:50 +0100

    Add core JSON viewer logic and style

--- src/json-explorer/demo.ts
+++ src/json-explorer/demo.ts
@@ -12,87 +12,43 @@ export const MOCK_JSON_DATA = {
 };
 
 export const MOCK_JSON_DATA_2 = {
-  date: "2021-10-27T07:49:14.896ZDE81200505501265402568DE81200505501265402568DE81200505501265402568DE81200505501265402568DE81200505501265402568DE81200505501265402568DE81200505501265402568DE81200505501265402568",
+  date: "2021-10-27T07:49:14.896ZDE",
   hasError: false,
+  object: {
+    id: "4c212130",
+    prop: "iban",
+    value: "DE81200505501265402568",
+    object: {
+      id: "4c212130",
+      prop: "iban",
+      value: "DE81200505501265402568",
+      object: {
+        id: "4c212130",
+        prop: "iban",
+        value: "DE81200505501265402568",
+        hasError: false,
+      },
+    },
+  },
   fields: [
-    {
-      id: "4c212130",
-      prop: "iban",
-      value: "DE81200505501265402568",
-      hasError: false,
-    },
-    {
-      id: "4c212130",
-      prop: "iban",
-      value: "DE81200505501265402568",
-      hasError: false,
-    },
-    {
-      id: "4c212130",
-      prop: "iban",
-      value: "DE81200505501265402568",
-      hasError: false,
-    },
-    {
-      id: "4c212130",
-      prop: "iban",
-      value: "DE81200505501265402568",
-      hasError: false,
-    },
-  ],
-  fields2: [
-    {
-      id: "4c212130",
-      prop: "iban",
-      value: "DE81200505501265402568",
-      hasError: false,
-      fields: [
-        {
+    "ok",
+    undefined,
+    null,
+    Symbol("foo"),
+    [
+      {
+        id: "4c212130",
+        prop: "iban",
+        value: "DE81200505501265402568",
+        hasError: false,
+        object: {
           id: "4c212130",
           prop: "iban",
           value: "DE81200505501265402568",
           hasError: false,
         },
-        {
-          id: "4c212130",
-          prop: "iban",
-          value: "DE81200505501265402568",
-          hasError: false,
-        },
-        {
-          id: "4c212130",
-          prop: "iban",
-          value: "DE81200505501265402568",
-          hasError: false,
-        },
-        {
-          id: "4c212130",
-          prop: "iban",
-          value: "DE81200505501265402568",
-          hasError: false,
-        },
-      ],
-    },
-    {
-      id: "4c212130",
-      prop: "iban",
-      value: "DE81200505501265402568",
-      hasError: false,
-    },
-    {
-      id: "4c212130",
-      prop: "iban",
-      value: "DE81200505501265402568",
-      hasError: false,
-    },
-    {
-      id: "4c212130",
-      prop: "iban",
-      value: "DE81200505501265402568",
-      hasError: false,
-    },
-  ],
-  fields3: [
+      },
+    ],
     {
       id: "4c212130",
       prop: "iban",
