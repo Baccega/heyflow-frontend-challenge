9c2f846ec3ba264dbce083eeb1b88dc5d637c89d
Sandro Baccega <mail.sandro.baccega@gmail.com>
2024-11-28 11:34:59 +0100

    Move JsonValue to own file, add selectedKey and improve styles

---
+++ src/json-explorer/components/JsonValue.tsx
@@ -0,0 +1,104 @@
+type JsonValueProps = {
+  data: Record<string, unknown>;
+  onKeyClick: (key: string) => void;
+};
+
+export default function JsonValue({ data, onKeyClick }: JsonValueProps) {
+  function renderValue(value: unknown) {
+    switch (true) {
+      case value === null:
+        return (
+          <div className="json-value-right json-value-right--null">
+            null
+            <span className="json-comma">,</span>
+          </div>
+        );
+      // I'm going to ignore Maps, Sets, ... for this demo
+      case typeof value === "object" && !Array.isArray(value):
+        return (
+          <>
+            <span className="json-bracket--left">&#123;</span>
+            <div className="json-value-right json-value-right--object">
+              <JsonValue
+                onKeyClick={onKeyClick}
+                data={value as Record<string, unknown>}
+              />
+            </div>
+            <span className="json-bracket--right">&#125;</span>
+            <span className="json-comma">,</span>
+          </>
+        );
+      case typeof value === "object" && Array.isArray(value):
+        return (
+          <>
+            <span className="json-bracket--left">&#91;</span>
+            <div className="json-value-right json-value-right--array">
+              {value.map((item, index) => (
+                <div className="json-value" key={index}>
+                  {renderValue(item)}
+                </div>
+              ))}
+            </div>
+            <span className="json-bracket--right">&#93;</span>
+            <span className="json-comma">,</span>
+          </>
+        );
+      case typeof value === "string":
+        return (
+          <div className="json-value-right json-value-right--string">
+            "{value}"<span className="json-comma">,</span>
+          </div>
+        );
+      case typeof value === "number":
+        return (
+          <div className="json-value-right json-value-right--number">
+            {value}
+            <span className="json-comma">,</span>
+          </div>
+        );
+      case typeof value === "boolean":
+        return (
+          <div className="json-value-right json-value-right--boolean">
+            {value ? "true" : "false"}
+            <span className="json-comma">,</span>
+          </div>
+        );
+      case typeof value === "undefined":
+        return (
+          <div className="json-value-right json-value-right--undefined">
+            undefined
+            <span className="json-comma">,</span>
+          </div>
+        );
+      // symbol, bigint, function, ...
+      default:
+        return (
+          <div className="json-value-right json-value-right--unknown">
+            {value.toString ? value.toString() : JSON.stringify(value)}
+            <span className="json-comma">,</span>
+          </div>
+        );
+    }
+  }
+
+  return (
+    <>
+      {data &&
+        Object.entries(data).map(([key, value], index) => {
+          const handleKeyClick = () => {
+            onKeyClick(key);
+          };
+
+          return (
+            <div key={index} className="json-value">
+              <span className="json-key" onClick={handleKeyClick}>
+                {key}
+              </span>
+              <span className="json-separator">:</span>
+              {renderValue(value)}
+            </div>
+          );
+        })}
+    </>
+  );
+}
--- src/json-explorer/components/JsonViewer.tsx
+++ src/json-explorer/components/JsonViewer.tsx
@@ -1,103 +1,33 @@
-function JsonValue({ data }: { data: Record<string, unknown> }) {
-  function renderValue(value: unknown) {
-    switch (true) {
-      case value === null:
-        return (
-          <div className="json-value-right json-value-right--null">
-            null
-            <span className="json-comma">,</span>
-          </div>
-        );
-      // I'm going to ignore Maps, Sets, ... for this demo
-      case typeof value === "object" && !Array.isArray(value):
-        return (
-          <>
-            <span className="json-bracket--left">&#123;</span>
-            <div className="json-value-right json-value-right--object">
-              <JsonValue data={value as Record<string, unknown>} />
-            </div>
-            <span className="json-bracket--right">&#125;</span>
-            <span className="json-comma">,</span>
-          </>
-        );
-      case typeof value === "object" && Array.isArray(value):
-        return (
-          <>
-            <span className="json-bracket--left">&#91;</span>
-            <div className="json-value-right json-value-right--array">
-              {value.map((item, index) => (
-                <div className="json-value" key={index}>
-                  {renderValue(item)}
-                </div>
-              ))}
-            </div>
-            <span className="json-bracket--right">&#93;</span>
-            <span className="json-comma">,</span>
-          </>
-        );
-      case typeof value === "string":
-        return (
-          <div className="json-value-right json-value-right--string">
-            "{value}"<span className="json-comma">,</span>
-          </div>
-        );
-      case typeof value === "number":
-        return (
-          <div className="json-value-right json-value-right--number">
-            {value}
-            <span className="json-comma">,</span>
-          </div>
-        );
-      case typeof value === "boolean":
-        return (
-          <div className="json-value-right json-value-right--boolean">
-            {value ? "true" : "false"}
-            <span className="json-comma">,</span>
-          </div>
-        );
-      case typeof value === "undefined":
-        return (
-          <div className="json-value-right json-value-right--undefined">
-            undefined
-            <span className="json-comma">,</span>
-          </div>
-        );
-      // symbol, bigint, function, ...
-      default:
-        return (
-          <div className="json-value-right json-value-right--unknown">
-            {value.toString ? value.toString() : JSON.stringify(value)}
-            <span className="json-comma">,</span>
-          </div>
-        );
-    }
-  }
-
-  return (
-    <>
-      {data &&
-        Object.entries(data).map(([key, value], index) => {
-          return (
-            <div key={index} className="json-value">
-              <span className="json-key">{key}</span>
-              <span className="json-separator">:</span>
-              {renderValue(value)}
-            </div>
-          );
-        })}
-    </>
-  );
-}
+import { useCallback, useState } from "react";
+import JsonValue from "./JsonValue";
 
 type JsonViewerProps = {
   data: Record<string, unknown>;
 };
 
 function JsonViewer(props: JsonViewerProps) {
+  const [selectedKey, setSelectedKey] = useState<string>();
+
+  const handleKeyClick = useCallback((key: string) => {
+    setSelectedKey(key);
+  }, []);
+
+  const selectedValue = props.data[selectedKey ?? ""];
+
+  console.log(selectedKey);
+
   return (
-    <div className="json-viewer">
-      <JsonValue data={props.data} />
-    </div>
+    <>
+      <div className="selected-key">
+        <h3>Key:</h3> <span>{selectedKey}</span>
+      </div>
+      <div className="selected-value">
+        <h3>Value:</h3> <span>{JSON.stringify(selectedValue)}</span>
+      </div>
+      <div className="json-viewer">
+        <JsonValue data={props.data} onKeyClick={handleKeyClick} />
+      </div>
+    </>
   );
 }
 
