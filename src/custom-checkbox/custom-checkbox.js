39d37a94be6c5cba8ae940f3729ad45f53197aef
Sandro Baccega <mail.sandro.baccega@gmail.com>
2024-11-29 13:03:13 +0100

    Use CSS for outline logic

--- src/custom-checkbox/custom-checkbox.js
+++ src/custom-checkbox/custom-checkbox.js
@@ -8,15 +8,3 @@ nativeCheckbox.addEventListener("change", function () {
 customCheckbox.addEventListener("change", function () {
   nativeCheckbox.checked = customCheckbox.checked;
 });
-
-nativeCheckbox.addEventListener("focusin", function () {
-  // Fallback for Firefox
-  customCheckbox.style.outlineColor = "blue";
-  customCheckbox.style.outlineColor = "-webkit-focus-ring-color";
-  customCheckbox.style.outlineStyle = "solid";
-  customCheckbox.style.outlineWidth = "2px";
-  customCheckbox.style.outlineOffset = "2px";
-});
-nativeCheckbox.addEventListener("focusout", function () {
-  customCheckbox.style.outline = "none";
-});
